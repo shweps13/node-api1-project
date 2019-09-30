51f250c99b36043959cc33e33f5b5f39943d3a3a
Heorhii Siburov <g.siburov@gmail.com>
2019-09-30 12:22:22 -0700

    initial setup - server listen 8000

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // implement your API here
+
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send('hello world');
+})
+
+const port = 8000;
+server.listen(port, () => console.log(`\n** API on port ${port} is working **\n`))
