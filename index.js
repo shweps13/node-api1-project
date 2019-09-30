4f45bbb2cb7b31917777009e84632e12e19c2be2
Heorhii Siburov <g.siburov@gmail.com>
2019-09-30 12:33:11 -0700

    GET by ID working now

--- index.js
+++ index.js
@@ -8,13 +8,30 @@ const server = express();
 
 server.use(express.json());
 
+// Testing of workability of the server
 server.get('/', (req, res) => {
     res.send('hello world');
 })
 
+// GET ALL users from database
 server.get('/api/users', (req, res) => {
 
     dataBase.find()
+    .then(users => {
+        res.send(users);
+    }).catch(error => {
+        res.send(error)
+    });
+
+});
+
+// GET user by ID from database
+server.get('/api/users/:id', (req, res) => {
+
+    const id = req.params.id;
+    console.log("ID of requested user: ", id)
+
+    dataBase.findById(id)
     .then(users => {
 
         res.send(users);
@@ -23,7 +40,6 @@ server.get('/api/users', (req, res) => {
     });
 
 });
-
 
 const port = 8000;
 server.listen(port, () => console.log(`\n** API on port ${port} is working **\n`))
