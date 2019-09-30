544a9573333837efb2e6ce782e63866f3de7fbb8
Heorhii Siburov <g.siburov@gmail.com>
2019-09-30 12:27:48 -0700

    GET method working

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // implement your API here
 
 const express = require('express');
+const dataBase = require('./data/db.js');
+
 
 const server = express();
 
@@ -9,6 +11,19 @@ server.use(express.json());
 server.get('/', (req, res) => {
     res.send('hello world');
 })
+
+server.get('/api/users', (req, res) => {
+
+    dataBase.find()
+    .then(users => {
+
+        res.send(users);
+    }).catch(error => {
+        res.send(error)
+    });
+
+});
+
 
 const port = 8000;
 server.listen(port, () => console.log(`\n** API on port ${port} is working **\n`))
